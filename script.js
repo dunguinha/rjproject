0a1c6aeea1f5986701a115facb42ef97ace16f06
Cristhian <99344786+dunguinha@users.noreply.github.com>
2023-01-20 18:58:58 -0300

    updatesla

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var i = setInterval(function () {
+var i = setInterval(function loading() {
     
     clearInterval(i);
   
     // O código desejado é apenas isto:
     document.getElementById("loading").style.display = "none";
-    document.getElementById("conteudo").style.display = "inline";
+    document.getElementById("conteudo").style.display = "none";
 
 }, 4000);
